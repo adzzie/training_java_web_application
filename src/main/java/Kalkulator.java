17bebe744c47c6b957d2ec7b45c962a7d6060d88
ghozal <ghozal@gmail.com>
2019-11-27 14:55:23 +0700

    kalkulator

---
+++ src/main/java/Kalkulator.java
@@ -0,0 +1,12 @@
+/**
+ * Description Kalkulator
+ *
+ * @author aji gojali
+ */
+public class Kalkulator {
+    public Integer tambah(Integer x, Integer y){
+        Integer a= x+y;
+        return a;
+    }
+
+}
